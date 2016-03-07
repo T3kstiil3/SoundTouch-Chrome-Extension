c1b795d05e4cc5486c30545250da9f5039557ea9
Loyer Aurélien <aurelien.loyer@zenika.com>
2016-03-07 18:02:59 +0100

    Ajout des boutons et de la preview

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SoundTouch-Chrome-Extension
 SoundTouch Chrome Extension, Display the current playing music, control the Soundtouch and more...
+
+# Preview
+![Preview img](img/preview.jpg)
--- widget/popup.js
+++ widget/popup.js
@@ -1,21 +1,53 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://10.0.10.166:8090/now_playing", true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    var resp = JSON.parse(xhr.responseText);
-    document.getElementById("text").innerText = resp;
-  }
-}
-xhr.send();
+document.addEventListener('DOMContentLoaded', function() {
 
-document.getElementById("title").innerText = "Test de Titre";
+  var xhr = new XMLHttpRequest();
 
-document.querySelector('#go-to-options').addEventListener(function() {
-  if (chrome.runtime.openOptionsPage) {
-    // New way to open options pages, if supported (Chrome 42+).
-    chrome.runtime.openOptionsPage();
-  } else {
-    // Reasonable fallback.
-    window.open(chrome.runtime.getURL('options/options.html'));
+  getNowPlaying();
+
+  //Now Playing display
+  function getNowPlaying(){
+    var url = 'http://10.0.10.166:8090/now_playing';
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        if (window.DOMParser)
+        {
+          parser=new DOMParser();
+          xmlDoc=parser.parseFromString(xhr.responseText,"text/xml");
+          var track = xmlDoc.getElementsByTagName("track")[0].childNodes[0].nodeValue;
+          var artist = xmlDoc.getElementsByTagName("artist")[0].childNodes[0].nodeValue;
+          var album = xmlDoc.getElementsByTagName("album")[0].childNodes[0].nodeValue;
+          var art = xmlDoc.getElementsByTagName("art")[0].childNodes[0].nodeValue;
+
+          document.getElementById("track").innerText = track;
+          document.getElementById("artist").innerText = artist;
+          document.getElementById("album").innerText = album;
+          document.getElementById("cover").src = art;
+        }
+      }
+    }
+    xhr.send();
   }
+
+  //Button List
+  var buttonRemote = document.getElementsByClassName("buttonRemote");
+  for (var i = 0; i < buttonRemote.length; i++) {
+    buttonRemote[i].addEventListener('click', sendRemoteButton, false);
+  }
+
+  //Send Remote Button with key parameter
+  function sendRemoteButton(){
+    var key = this.value;
+    var url = 'http://10.0.10.166:8090/key';
+    xhr.open("POST", url, true);
+    xhr.send('<?xml version="1.0" encoding="UTF-8" ?><key state="press" sender="Gabbo">'+key+'</key>');
+    setTimeout(function() { getNowPlaying(); }, 2000);
+  }
+
+  //GO to Settings
+  var settings_btn = document.getElementById('settings_btn');
+  settings_btn.addEventListener('click', function() {
+    console.log('gotosettings');
+  });
+
 });
