2aa8e0e6b0f86f2fbc96159632649504f677eeda
Loyer Aurélien <aurelien.loyer@zenika.com>
2016-12-19 14:26:56 +0100

    #5 Zip project files task create

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+ 
+gulp.task('zip', () => {
+    return gulp.src(['!node_modules','!node_modules/*','!dist','!dist/*','!main.js','!gulpfile.js','*','*/*'])
+        .pipe(zip(renderFileName()))
+        .pipe(gulp.dest('../'));
+});
+
+
+function renderFileName(){
+    var utc = new Date().toJSON().slice(0,10);
+    return 'SoundTouch-Chrome-Extension-'+utc+'.zip';
+}
